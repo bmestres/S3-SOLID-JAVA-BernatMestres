5661757bcef4e96a19ce4d433967929cfde9adf4
Bernat Mestres <ber.mestres@gmail.com>
2026-08-13 17:19:33 +0200

    initial commit

--- Tasca-S3-0/I/src/WashingMachine.java
+++ Tasca-S3-0/I/src/WashingMachine.java
@@ -10,16 +10,17 @@ public class WashingMachine implements MachineActions {
         System.out.println("WashingMachine is OFF");
     }
 
+    // segregate to new heater interface
     @Override
     public void heat() {
         System.out.println("Heat operation does not apply to WashingMachine.");
     }
-
+    // segregate to new cooler interface
     @Override
     public void cool() {
         System.out.println("Cool operation does not apply to WashingMachine.");
     }
-
+    // segregate to new washer interface
     @Override
     public void wash() {
         System.out.println("WashingMachine is washing clothes.");
